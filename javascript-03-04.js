6ffff0927f112fe082155bcaeed7de827317e5c0
peter L <peter.l@uvaper.com>
2019-05-25 18:50:56 +0900

    끝말 잇기 공부

--- javascript-03-04.js
+++ javascript-03-04.js
@@ -2,6 +2,8 @@ var 바디 = document.body;
 var 단어 = document.createElement('div');
 단어.textContent = "이열프"
 document.body.append(단어);
+var 폼 = document.createElement('form');
+폼.append(입력창);
 var 입력창 =  document.createElement('input');
 document.body.append(입력창);
 var 버튼 = document.createElement('button');
@@ -10,13 +12,16 @@ document.body.append(버튼);
 var 결과창 = document.createElement('div');
 document.body.append(결과창);
 
-버튼.addEventListener('click',function 콜백함수(){
+버튼.addEventListener('click',function 콜백함수 (){
 
     if (단어.textContent[단어.textContent.length -1] === 입력창.value[0]) { //입력창.valeu === 이열프
-            alert('딩동생');
-            word = answer ;
+            결과창.textContent = '딩동댕';
+            단어.textContent = 입력창.value;
+            입력창.value = '';
+            입력창.focus();
         }else {
-            alert ("땡");
+            결과창.textContent = '땡';
+            입력창.focus();
         }
 
 }); //콜백함수
